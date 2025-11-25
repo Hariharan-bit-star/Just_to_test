47fce278b549b6531d36932b38621906bb187a5a
Hariharan-bit-star <qa-hariharan@twentyideas.com>
2025-11-26 01:33:04 +0530

    Commit for java

---
+++ TryExample.java
@@ -0,0 +1,9 @@
+class TryExample{
+    public static void main (String args [])
+    {
+    int Hari = 7;
+    String Batman = "hariharan";
+    System.out.print(Hari);
+    System.out.print (Batman);
+    }
+}
